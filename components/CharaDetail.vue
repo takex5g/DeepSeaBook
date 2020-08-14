a668bc96210a351846d1ce09bb91b7e1ec3dae49
takex5g <takex5g@gmail.com>
2020-08-14 23:32:59 +0900

    説明付きキャラコンポーネント動作

---
+++ components/CharaDetail.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="chara" :class="position">
+    <img :src="require('@/assets/img/chara/'+name+'.png')" />
+    <div>
+      <h3>
+        <slot name="name">名前が設定されていません</slot>
+      </h3>
+      <p>
+        <slot name="desc">説明文が設定されていません</slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import anime from "animejs"; // animejsの読み込み
+export default {
+  props: {
+    position: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+  },
+  data: function () {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+};
+</script>
+
+<style scoped>
+.chara {
+  max-width: 100%;
+  max-height: 100%;
+  display: grid;
+  grid-column: 1 / -1;
+}
+.chara img {
+  height: 100px;
+  width: auto;
+}
+.chara div {
+  /*回り込み説明文付き */
+  text-align: left;
+}
+
+/**全体ボックスの設定 */
+.left {
+  /*回り込み説明文付き */
+  grid-template-columns: auto 1fr;
+}
+.center {
+  /* grid-template-columns: 1fr auto; */
+}
+.right {
+  grid-template-columns: 1fr auto;
+}
+/*ここまで*/
+
+/*名前説明の設定*/
+.left div {
+  margin-left: 10px;
+  text-align: left;
+}
+.right div {
+  margin-right: 10px;
+  text-align: right;
+}
+.center div {
+  text-align: center;
+}
+/**ここまで */
+
+/*画像の設定*/
+.right img {
+  order: 2; /*順番を入れかえ */
+}
+.left img {
+}
+.center img {
+  margin: auto;
+}
+/**ここまで */
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,28 @@
     <Background class="background" :scrollY="scrollY" :size="size"></Background>
     <div class="contents">
       <h1>魅惑の深海図鑑</h1>
-      {{size.width}},{{size.height}}
-      <Chara name="mendako" position="center">
+      <!-- {{size.width}},{{size.height}} -->
+      <CharaDetail name="mendako" position="left" style="grid-row: 2 / span 1;">
         <template #name>メンダコ</template>
-        こんにちは深海魚
-      </Chara>
-
-      <!-- <br v-for="i in 500" :key="i" /> -->
+        <template #desc>こんにちは深海魚ここは説明文</template>
+      </CharaDetail>
+      <CharaDetail name="mendako" position="right" style="grid-row: 4 / span 1;">
+        <template #name>メンダコ</template>
+        <template #desc>こんにちは深海魚うぇいうぇい</template>
+      </CharaDetail>
+      <CharaDetail name="mendako" position="center" style="grid-row: 6 / span 1;">
+        <template #name>メンダコ</template>
+        <template #desc>こんにちは深海魚うぇいうぇい</template>
+      </CharaDetail>
     </div>
   </div>
 </template>
 
 <script>
 import Background from "@/components/Background";
-import Chara from "@/components/Chara";
+import CharaDetail from "@/components/CharaDetail";
 export default {
-  components: { Background, Chara },
+  components: { Background, CharaDetail },
   data: function () {
     return {
       scrollY: 0,
@@ -80,11 +86,14 @@ export default {
   background-color: rgba(0, 0, 0, 0.055);
   text-align: center;
   display: grid;
-  grid-template-rows: repeat(100, 1fr);
-  grid-template-columns: auto auto auto auto;
+  grid-template-rows: repeat(100, 100px);
+  grid-template-columns: repeat(6, 1fr);
+
+  padding-left: 20px;
+  padding-right: 20px;
 }
 h1 {
-  grid-column: 1 / 5;
+  grid-column: 1 / -1;
   margin: 0;
 }
 </style>
